07b88e0ce937e517872e8f54ad6fc06d59444448
Vinicius Ducatti Ferraz <110203525+vinismo@users.noreply.github.com>
2023-09-03 18:27:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,52 +7,69 @@ def incluirContato():
     telefone = input("Digite o número telefone do contato: ")
     contato = {"Nome": nome, "Telefone": telefone}
     contatos.append(contato)
+    print("\n")
     print("Contato adicionado!")
+    print("-------------------")
 
 #pesquisar contato
 def pesquisarContato():
-    nome = input("Digite o nome do contato: ")
+    nome = input("Digite o nome do contato: ").upper()
     encontrado = False
     for contato in contatos:
         if contato["Nome"] == nome:
+            print("\n")
+            print("*********************************")
             print(f"Nome: {contato['Nome']}, Telefone: {contato['Telefone']}")
+            print("*********************************")
             encontrado = True
             break
     if not encontrado:
+        print("\n")
         print("Contato não encontrado.")
+        print("-----------------------")
 
 #atualizar contato
 def atualizarContato():
-    nome = input("Digite o nome do contato: ")
+    nome = input("Digite o nome do contato: ").upper()
     for contato in contatos:
         if contato["Nome"] == nome:
             novo_telefone = input("Digite o novo número de telefone: ")
             contato["Telefone"] = novo_telefone
+            print("\n")
             print("Contato atualizado!")
+            print("-------------------")
             return
+    print("\n")
     print("Contato não localizado.")
+    print("-----------------------")
 
 #excluir contato
 def excluirContato():
-    nome = input("Digite o nome do contato: ")
+    nome = input("Digite o nome do contato: ").upper()
     for contato in contatos:
         if contato["Nome"] == nome:
             contatos.remove(contato)
+            print("\n")
             print("Contato excluído!")
+            print("-----------------")
             return
-    print("Contato não encontrado.")
+    print("\n")
+    print("Contato não encontrado!")
+    print("-----------------------")  
 
 #listar contatos (ordem alfabetica)
 def listarContatos():
     if not contatos:
         print("\n")
-        print("Sua agenda não possui contatos.")
+        print("Sua agenda não possui contatos!")
+        print("-------------------------------")      
     else:
         print("\n")
         print("Lista de Contatos:")
         contatos_ordenados = sorted(contatos, key=lambda x: x["Nome"]) #ordem alfabetica
         for contato in contatos_ordenados:
             print(f"Nome: {contato['Nome']}, Telefone: {contato['Telefone']}")
+            print("\n")
 
 #principal
 while True:
@@ -84,7 +101,9 @@ while True:
     elif escolha == "6":
         print("\n")        
         print("Tchau, querido professor!")
+        print("-------------------------")
         break
     else:
         print("\n")
         print("Opção inválida, tente novamente.")
+        print("--------------------------------")
